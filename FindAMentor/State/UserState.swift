73c06b6c8fb4ddcc99fb1a27ef92571e27d707be
Riccardo Cipolleschi <rc@bendingspoons.com>
2020-04-11 17:13:58 +0200

    feat: add userState

---
+++ FindAMentor/State/UserState.swift
@@ -0,0 +1,20 @@
+//
+//  UserState.swift
+//  FindAMentor
+//
+//  Created by Riccardo Cipolleschi on 11/04/2020.
+//
+
+import Foundation
+import Katana
+
+extension AppState {
+
+  /// This structure contains all the informations related to the user.
+  struct UserState: State, Codable {
+    // TODO: study the backend API
+    // the user state will be very complex, given that it should handle
+    // mentors, mentorees, goals, requests, ...
+    // The suggestion is to split it in different slices.
+  }
+}
